91b049df761c1a30a8b99f7652bc654f6ccc7ba7
Anthony Lee <hososlch@gmail.com>
2022-10-31 18:18:39 +0800

    fix: file name

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-function App() {
-    return <div className="App">AAA</div>;
-}
+import React from "react";
 
-export default App;
+export const App = React.memo(() => {
+    return <div className="App">AAA</div>;
+});
